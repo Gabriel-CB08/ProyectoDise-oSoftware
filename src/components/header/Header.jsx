c0a94818ddca5283f381ec558b548a4b5cc728cf
Gabriel-CB08 <143332610+Gabriel-CB08@users.noreply.github.com>
2023-11-26 21:13:09 -0300

    a

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
       <img className="header__logo" src="https://www.svgrepo.com/show/530571/conversation.svg" alt="" />
       <div className="primary-nav">
         <a href="https://compralotodo.netlify.app">Compralotodo    </a>
-        <a href="https://compralotodo.netlify.app">Contactanos   </a>
-        <a href="">Gamersssss   </a>
+        <a href="https://unrivaled-tarsier-ae76e5.netlify.app/">Contactanos   </a>
+        <a href="https://compralotodohistoria.netlify.app/">Historia</a>
       </div>
     </header>
   );
